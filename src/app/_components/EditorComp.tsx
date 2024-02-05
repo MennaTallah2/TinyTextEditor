fcc25d8b4577fd81db2d40fe3e2ed06552761406
Mennatullah Ibraheem <mennaibraheem2@gmail.com>
2024-02-05 17:09:51 +0200

    tinymce with hosting

---
+++ src/app/_components/EditorComp.tsx
@@ -0,0 +1,63 @@
+import React, { useRef } from "react";
+import { Editor } from "@tinymce/tinymce-react";
+import { TinyMCE } from "tinymce";
+
+export default function EditorComp() {
+  const editorRef = useRef<any>();
+  const log = () => {
+    if (editorRef.current) {
+      console.log(editorRef.current.getContent());
+    }
+  };
+  return (
+    <div className="w-full h-full">
+      <Editor
+        tinymceScriptSrc={"http://localhost:3000/" + "tinymce/tinymce.min.js"}
+        onInit={(evt, editor) => (editorRef.current = editor)}
+        initialValue=""
+        init={{
+          height: 500,
+          menubar: true,
+          plugins: ["table", "image", "code"],
+          toolbar:
+            "undo redo | blocks | link image | code | ßtable " +
+            "bold italic forecolor | alignleft aligncenter " +
+            "alignright alignjustify | bullist numlist outdent indent | " +
+            "removeformat | help",
+          table_toolbar:
+            "tableprops tabledelete | tableinsertrowbefore tableinsertrowafter tabledeleterow | tableinsertcolbefore tableinsertcolafter tabledeletecol",
+          content_style:
+            "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
+          table_appearance_options: false,
+          file_picker_types: "image",
+          image_title: true,
+          automatic_uploads: true,
+          file_picker_callback: (cb, value, meta) => {
+            const input = document.createElement("input");
+            input.setAttribute("type", "file");
+            input.setAttribute("accept", "image/*");
+            input.addEventListener("change", (e: any) => {
+              const file = e.target.files[0];
+              const reader = new FileReader();
+              reader.addEventListener("load", () => {
+                const id = "blobid" + new Date().getTime();
+                const blobCache = (tinymce as TinyMCE).activeEditor.editorUpload
+                  .blobCache;
+                const base64 = reader.result.split(",")[1];
+                const blobInfo = blobCache.create(id, file, base64);
+                blobCache.add(blobInfo);
+                cb(blobInfo.blobUri(), { title: file.name });
+              });
+              reader.readAsDataURL(file);
+            });
+
+            input.click();
+          },
+        }}
+      />
+      <button className="m-3 bg-slate-100 p-2 rounded-sm " onClick={log}>
+        Log editor content
+      </button>
+    </div>
+  );
+}
